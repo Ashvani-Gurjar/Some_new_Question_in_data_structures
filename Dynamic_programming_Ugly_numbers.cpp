9b0f36e75664255ae7e5d9d5c58e8a079969fd96
Ashvani-Gurjar <singhashvani82@gmail.com>
2022-10-09 05:48:26 -0700

    some new problems:

---
+++ Dynamic_programming_Ugly_numbers.cpp
@@ -0,0 +1,24 @@
+// In that Question Ugly number is that number which 
+// factor are 2,3,5 is present 
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     int n;
+     cin>>n;
+     int c2(0),c3(0),c5(0);
+     vector<int>dp(n+1);
+     dp[0]=1;
+     for(int i=1;i<=n;i++){
+          dp[i]=min({2*dp[c2],3*dp[c3],5*dp[c5]});
+          if(2*dp[c2]==dp[i])
+             c2++;
+          if(3*dp[c3]==dp[i])
+             c3++;
+          if(5*dp[c5]==dp[i])
+             c5++;
+     }
+     cout<<dp[n-1]<<endl;
+
+return 0;
+}
