aca3d43ead554b55db13746ec799f1a2a9a651ee
Ashvani-Gurjar <singhashvani82@gmail.com>
2022-10-10 06:55:44 -0700

    Fermat_little_and_willson_Therom_number

---
+++ Fermat_little_and_wilson_Therom_number_theory.cpp
@@ -0,0 +1,48 @@
+ //  ---- >> Modulo inverse using Fermat's Theorem <<<-----
+//  aP= a(mod p)
+//  if a is not divisible by p, 
+//             aP-1 = 1(mod p)
+//   Multiplying both sides by a-1 
+//             aP-2 =a-1(mod P)          
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long 
+const int N =1e5+2, MOD = 1e9+7;
+int power(int x,int y,int p){
+     int res =1;
+     x=x%p;
+     while(y>0){
+          if(y&1){
+               res =(res *x)%p;
+               y=y>>1;
+               x=(x*x)%p;
+          }
+        
+     }
+       return res;
+}
+int modInverse(int a,int p){
+      return power(a,p-2,p);
+}
+
+
+int modfact(int n,int p)
+{
+     if(p<=n){
+          return 0;
+     }
+     int res =(p-1);
+     for(int i=n+1;i<p;i++){
+          res=(res*modInverse(i,p))%p;
+     }
+     return res;
+}
+signed main(){
+     int n,p;
+     cin>>n>>p;
+     cout<<modfact(n,p)<<endl;
+
+return 0;
+}
