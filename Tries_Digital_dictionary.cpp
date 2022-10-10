c589ef985d8dc4c3846900f39d24fa3461a551c2
Ashvani-Gurjar <singhashvani82@gmail.com>
2022-10-10 05:05:21 -0700

    Some new Trie problem

---
+++ Tries_Digital_dictionary.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+class node{
+     public:
+     node*next[26];
+     bool end;
+     node(){
+          for(int i=0;i<26;i++){
+               next[i] =NULL;
+          }
+          end =false;
+     }
+};
+class Trie{
+     private:
+       node*root;
+     public:
+       Trie(){
+          root =new node();
+       }
+       void insert(string &s){
+          node *it =root;
+          for(auto c:s){
+               if(it->next[c-'a'])
+                it->next[c-'a'] =new node();
+                it =it->next[c -'a'];
+          }  
+           it->end =true;
+       }
+       void find(string &s){
+          node*it = root;
+          for(auto c: s){
+               if(!it->next[c-'a']){
+                    cout<<"No suggestions"<<endl;
+                    insert(s);
+                    return ;
+               }
+               it =it->next[c-'a'];
+          }
+          vector<string>res;
+          printall(it,s,res,"");
+          for(auto c:res){
+               cout<<s<<c<<endl;
+          }
+       }
+       void printall(node* it,string &s ,vector<string>&res, string curr){
+          if(it==NULL)
+          return;
+          if(it->end){
+               res.push_back(curr);
+          }
+          for(int i=0;i<26;i++){
+               if(it->next[i]){
+                    printall(it->next[i],s,res,curr+char('a'+i));
+               }
+          }
+       }
+};
+
+int main(){
+     Trie t;
+     int n;
+     cin>>n;
+     vector<string >a(n);
+     for(auto &i :a){
+          cin>>i;
+          t.insert(i);
+     }
+     int q;
+     cin>>q;
+     while(q--){
+          string s;
+          cin>>s;
+          t.find(s);
+     }
+     
+
+return 0;
+}
